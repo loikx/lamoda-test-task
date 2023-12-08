20d8c308f25e3370381acd5b6145080f32c414e7
loikx <vartleon987@gmail.com>
2023-12-08 23:44:46 +0300

    feat: add main func

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/lamoda-tech/loikx/internal/app"
+)
+
+func main() {
+	application := app.NewApp()
+	if err := application.Init(context.Background()); err != nil {
+		log.Fatalf("%v\n", err)
+	}
+
+	if err := application.Start(); err != nil {
+		log.Fatalf("%v\n", err)
+	}
+}
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,4 +1,4 @@
-package fridge
+package app
 
 import (
 	"context"
