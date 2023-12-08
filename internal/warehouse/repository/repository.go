44f9944bc456e2fba5e6fc0aa2237bcb2c536bd5
loikx <vartleon987@gmail.com>
2023-12-08 23:30:24 +0300

    refactor: add repository

--- internal/warehouse/repository/repository.go
+++ internal/warehouse/repository/repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"sync"
 
 	"github.com/gofrs/uuid"
 	"github.com/jackc/pgx/v5"
@@ -10,8 +11,23 @@ import (
 
 type WareHouseRepository struct {
 	con *pgx.Conn
+
+	mu sync.Mutex
 }
 
 func (r *WareHouseRepository) FindByID(ctx context.Context, id uuid.UUID) ([]domain.Product, error) {
-	panic("implement me")
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	var items []domain.Product
+
+	err := r.con.QueryRow(
+		ctx,
+		"select * from product p where exists(select 1 from warehouse w where w.id = p.warehouse_id and not p.is_reserved)",
+	).Scan(&items)
+	if err != nil {
+		return nil, err
+	}
+
+	return items, nil
 }
